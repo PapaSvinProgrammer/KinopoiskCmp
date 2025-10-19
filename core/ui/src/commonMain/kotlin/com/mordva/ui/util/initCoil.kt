b2e0cd5ce9373682a59ce57f148418408ee8bcb0
romanurajkin <UraykinRab@yandex.ru>
2025-10-19 12:53:13 +0300

    Fixed bug with coil

--- core/ui/src/commonMain/kotlin/com/mordva/ui/util/initCoil.kt
+++ core/ui/src/commonMain/kotlin/com/mordva/ui/util/initCoil.kt
@@ -5,20 +5,14 @@ import coil3.ImageLoader
 import coil3.PlatformContext
 import coil3.annotation.ExperimentalCoilApi
 import coil3.compose.setSingletonImageLoaderFactory
-import coil3.network.ktor3.KtorNetworkFetcherFactory
 import coil3.request.crossfade
-import coil3.util.DebugLogger
 
 @Composable
 @OptIn(ExperimentalCoilApi::class)
 fun InitCoil() {
     setSingletonImageLoaderFactory { context: PlatformContext ->
         ImageLoader.Builder(context)
-            .components {
-                add(KtorNetworkFetcherFactory())
-            }
             .crossfade(true)
-            .logger(DebugLogger())
             .build()
     }
 }
